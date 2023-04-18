d048c7e5c12bf9e9d606cffd81fc475f111321d1
ZzinzZ <nhata338@gmail.com>
2023-04-18 12:11:22 +0700

    login

---
+++ login.js
@@ -0,0 +1,11 @@
+const registerButton = document.getElementById("register-nav");
+const loginButton = document.getElementById("login-nav");
+const container = document.getElementById("container");
+
+registerButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+loginButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
